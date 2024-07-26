ce6a3020322b474035b041bb4251a91bb8452764
Yuheng <wu459@wisc.edu>
2024-07-26 14:24:08 +0000

    Train on head

--- val-affordance.py
+++ val-affordance.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO('runs/segment/affordance-nomosaic/weights/best.pt')
+model = YOLO('runs/segment/affordance-lr0.001/weights/best.pt')
 
 result = model.val(data='affordance.yaml', device = 0, plots=False)
 
 print("mAP:", result.seg.map)
 print("mAP50: ", result.seg.map50)
 print("mAP75: ", result.seg.map75)
+print("mAPs:", result.seg.maps)
