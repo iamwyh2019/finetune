cfa12d62307713c06c3df1876241395857722665
Yuheng <wu459@wisc.edu>
2024-07-30 14:18:20 +0000

    Reindex vistas dataset

--- finetune.py
+++ finetune.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     # train with lower LR (lr0=0.0001, momentum=0.9)
     # freeze the backbone (the first 10 layers)
     model.train(data='data-vistas-removed.yaml', pretrained=True, epochs=150, device=0, exist_ok=True,
-                freeze=10, cache=True, name='vistas-lr0.001', mosaic=0, lr0=0.001, momentum=0.9,
+                freeze=10, cache=True, name='vistas-lr0.002', mosaic=0, lr0=0.002, momentum=0.9,
                 optimizer='SGD')
